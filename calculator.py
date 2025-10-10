b01cee97fd7dd18ada538d828b2b2aba27fb5251
emeritusmustapha <mmustaphamuhammad01@gmail.com>
2025-10-10 20:52:17 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
-firstnumber = input(float("type in the first number"))
+firstnumber = input(float("type in the first number : "))
+secondnumber = input(float("type in the second number : "))
+
+operation = input("type in the operation you want +,-,*,/ : ")
+
+if operation = '+':
+  print(firstnumber + secondnumber)
+
+elif operation = '-':
+  print(firstnumber - secondnumber)
+elif operation = '*':
+  print(firstnumber * secondnumber)
+elif operation = '/':
+  print(firstnumber / secondnumber)
+else:
+  print("invalid parameter")
+
